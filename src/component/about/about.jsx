b194fc9083c3d0cf8349afd2bffd7139dd25c51c
Missmiesharh <ayshamustapha99@gmail.com>
2023-02-14 14:27:29 +0100

    hi

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -1,4 +1,5 @@
 import Navbar from '../navbar/Nav'
+import Footer from '../footer/Footer'
 import './about.css'
 
 const About = () => {
@@ -8,6 +9,17 @@ const About = () => {
         {<Navbar />}
         <h2>We love writing</h2> 
       </div>
+      <div className="mid-section container">
+        <div className='about_pic'>
+          <img src='https://assets.website-files.com/5e4b1929fccc7f2575de825e/5e4b1929fccc7f1c02de82bd_looking-to-lake-prof.jpg' alt='about pic' />
+        </div>
+        <div className="about-text">
+          <p>The rich text element allows you to create and format headings, paragraphs, blockquotes, images, and video all in one place instead of having to add and format them individually. Just double-click and easily create content. </p>
+          <p>A rich text element can be used with static or dynamic content. For static content, just drop it into any page and begin editing. For dynamic content, add a rich text field to any collection and then connect a rich text element to that field in the settings panel. Voila! </p>
+          <p>Headings, paragraphs, blockquotes, figures, images, and figure captions can all be styled after a class is added to the rich text element using the "When inside of" nested selector system.</p>
+        </div>
+      </div>
+      {<Footer />}
     </>
    
   )
--- src/component/contact/contact.jsx
+++ src/component/contact/contact.jsx
@@ -1,11 +1,70 @@
 import Navbar from '../navbar/Nav'
+import Footer from '../footer/Footer'
+import './contact.css'
+import { AiOutlineTwitter, AiFillFacebook, AiFillInstagram } from 'react-icons/ai'
 
 const contact = () => {
   return (
-    <div>
-    {<Navbar />}
-    <h2>contact</h2>
-</div>
+    <>
+      <div className='contact_container container'>
+        {<Navbar />}
+        <h2>Let's Connect</h2>
+      </div>
+
+      <div className="mid l container">
+        <div className="connect">
+          <div className="contact-text">
+            <h3>Connect with Us</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum tristique.
+              Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+              Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.
+              Nunc ut sem vitae risus tristique posuere.</p>
+          </div>
+
+
+          <div className='contact-socials'>
+            <div className="l">
+              <AiOutlineTwitter />
+              <p>Follow us on Twitter</p>
+            </div>
+
+            <div className="l">
+              <AiFillFacebook />
+              <p>Follow us on Twitter</p>
+            </div>
+
+            <div className="l">
+              <AiFillInstagram />
+              <p>Follow us on Twitter</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="form">
+          <div className="form-text">
+            <h2>Send Us A Message</h2>
+          </div>
+          <div className="form-info ">
+            <div className="form-item">
+              <label>Name:<input type='text' placeholder='Enter Your Name' /></label>
+            </div>
+
+            <div className="form-item">
+              <label>Email Address: <input type='text' placeholder='Enter Your Email' /></label>
+            </div>
+
+            <div className="form-item">
+              <label>Message: <textarea placeholder='Say what you need to say' /></label>
+            </div>
+
+            <button type='submit' className='btn btn-primary'> Submit</button>
+          </div>
+        </div>
+      </div>
+
+      {<Footer />}
+    </>
   )
 }
 
